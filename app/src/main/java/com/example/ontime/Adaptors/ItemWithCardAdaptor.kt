e9786d0840fb4d7db819b098295ad00262fa24d9
fathy <deadcode61@gmail.com>
2022-07-18 17:33:07 +0200

    created item view card  with adaptor

--- app/src/main/java/com/example/ontime/Adaptors/ItemWithCardAdaptor.kt
+++ app/src/main/java/com/example/ontime/Adaptors/ItemWithCardAdaptor.kt
@@ -1,4 +1,26 @@
 package com.example.ontime.Adaptors
 
-class ItemWithCardAdaptor {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ontime.R
+
+class ItemWithCardAdaptor(private val itemWithCardViewItemList: List<String>) :
+    RecyclerView.Adapter<ItemWithCardAdaptor.MyItemWithCardViewHolder>() {
+    inner class MyItemWithCardViewHolder(item: View) : RecyclerView.ViewHolder(item)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyItemWithCardViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_with_card, parent, false)
+        return MyItemWithCardViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyItemWithCardViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return 0
+    }
 }
