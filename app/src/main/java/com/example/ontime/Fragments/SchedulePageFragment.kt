e80751bf6c18863ea23df8f60bb0224a6242b04b
fathy <deadcode61@gmail.com>
2022-07-19 03:20:19 +0200

    view card

--- app/src/main/java/com/example/ontime/Fragments/SchedulePageFragment.kt
+++ app/src/main/java/com/example/ontime/Fragments/SchedulePageFragment.kt
@@ -6,28 +6,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.ontime.Adaptors.ItemWithCardAdaptor
+import com.example.ontime.DataClasses.ItemWithCardData
+import com.example.ontime.MainActivity
 import com.example.ontime.R
 import com.example.ontime.databinding.FragmentSchedulePageBinding
 
+private var item = arrayListOf(
+    ItemWithCardData("me", " 4:55pm", "250", "cairo"),
+    ItemWithCardData("me", " 4:55pm", "250", "cairo")
+)
+
 class SchedulePageFragment : Fragment() {
+
     lateinit var binding: FragmentSchedulePageBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
         binding = FragmentSchedulePageBinding.inflate(inflater, container, false)
+        myCardItem()
+         customCalender()
+
         return binding.root
     }
 
 
-    @SuppressLint("ResourceAsColor")
+    @SuppressLint("ResourceType")
     private fun customCalender() {
         binding.cvDate.apply {
-            setBackgroundColor(R.color.white)
-            setDate(1L, true, true)
-
+            rootView.setBackgroundColor(R.color.black)
         }
+    }
 
+
+    private fun myCardItem() {
+        val adaptor = ItemWithCardAdaptor(item)
+        binding.rcItem.layoutManager = LinearLayoutManager(activity as MainActivity)
+        adaptor.notifyItemInserted(item.size - 1)
     }
 }
