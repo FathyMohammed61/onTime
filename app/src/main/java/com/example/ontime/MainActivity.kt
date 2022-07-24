63705211179279f5aa9119d7db0d89d0d03e00c6
FathyMohammed61 <105972741+FathyMohammed61@users.noreply.github.com>
2022-07-24 03:16:47 +0200

    change new fragment

--- app/src/main/java/com/example/ontime/MainActivity.kt
+++ app/src/main/java/com/example/ontime/MainActivity.kt
@@ -6,18 +6,18 @@ import androidx.fragment.app.Fragment
 import com.example.ontime.Fragments.LoginAppFragment
 import com.example.ontime.Fragments.SchedulePageFragment
 import com.example.ontime.databinding.ActivityMainBinding
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     var loginAppFragment = LoginAppFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         setTheme(R.style.Theme_OnTime)
-        setCurrentFragment(SchedulePageFragment())
+        setCurrentFragment(loginAppFragment)
     }
 
     private fun setCurrentFragment(fragment: Fragment) {
