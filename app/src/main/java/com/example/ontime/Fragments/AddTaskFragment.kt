3fea86a8576b688716256750153df5e4515d2052
FathyMohammed61 <105972741+FathyMohammed61@users.noreply.github.com>
2022-07-29 14:31:52 +0200

    to here good

--- app/src/main/java/com/example/ontime/Fragments/AddTaskFragment.kt
+++ app/src/main/java/com/example/ontime/Fragments/AddTaskFragment.kt
@@ -1,55 +1,51 @@
 package com.example.ontime.Fragments
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.ontime.R
+import com.example.ontime.databinding.FragmentAddTaskBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class AddTaskFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentAddTaskBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_task, container, false)
+    ): View {
+        binding = FragmentAddTaskBinding.inflate(inflater)
+      passingData()
+        return binding.root
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddTaskFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddTaskFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun passingData() {
+        val text = binding.addText.text.toString()
+        val date = binding.addDate.text.toString()
+        val notes = binding.addNotes.text.toString()
+        val time = binding.addTime.text.toString()
+        val place = binding.addPlace.text.toString()
+        val checkedRemember = binding.cbRemember
+        val btnCreateTask = binding.createTask
+
+        btnCreateTask.setOnClickListener {
+
+                Intent().putExtra("addText", text)
+                Intent().putExtra("addDate", date)
+                Intent().putExtra("addNotes", notes)
+                Intent().putExtra("addTime", time)
+                Intent().putExtra("addPlace", place)
+                Intent().putExtra("cbRemember", false)
+
+            parentFragmentManager.beginTransaction().also {
+                it.replace(R.id.flMain, AddTaskFragment())
+                it.commit()
             }
+        }
     }
 }
--- app/src/main/java/com/example/ontime/Fragments/SchedulePageFragment.kt
+++ app/src/main/java/com/example/ontime/Fragments/SchedulePageFragment.kt
@@ -1,10 +1,12 @@
 package com.example.ontime.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
@@ -13,11 +15,37 @@ import com.example.ontime.Adaptors.ItemWithCardAdaptor
 import com.example.ontime.DataClasses.ItemWithCardData
 import com.example.ontime.R
 import com.example.ontime.databinding.FragmentSchedulePageBinding
+import com.google.android.material.textfield.TextInputEditText
 
 
 class SchedulePageFragment : Fragment() {
 
     private lateinit var binding: FragmentSchedulePageBinding
+/*
+"addText", t
+"addDate", d
+"addNotes",
+"addTime", t
+"addPlace",
+"cbRemember"
+ */
+
+    val addText: TextInputEditText? = view?.findViewById(R.id.add_text)
+    val addNotes: TextInputEditText? = view?.findViewById(R.id.add_notes)
+    val addPlace: TextInputEditText? = view?.findViewById(R.id.add_place)
+    val addTime: TextInputEditText? = view?.findViewById(R.id.add_time)
+    val addDate: TextInputEditText? = view?.findViewById(R.id.add_date)
+    val checkRemember: CheckBox? = view?.findViewById(R.id.cb_remember)
+
+
+
+    val addedText = Intent().getStringExtra("addText").toString()
+    val addedDate = Intent().getStringExtra("addDate").toString()
+    val addedNotes = Intent().getStringExtra("addNotes").toString()
+    val addedTime = Intent().getStringExtra("addTime").toString()
+    val addedPlace = Intent().getStringExtra("addPlace").toString()
+    val checkedOr = Intent().getStringExtra("cbRemember")
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,20 +59,26 @@ class SchedulePageFragment : Fragment() {
     }
 
     private fun myCardItem() {
+
+
         val item = arrayListOf<ItemWithCardData>(
-            ItemWithCardData("me", " 4:55pm", "250", "cairo", true),
-            ItemWithCardData("me", " 4:55pm", "250", "cairo", false),
-            ItemWithCardData("ewewewewe", " 4:55pm", "250", "sdsdfdfdfds", true),
-            ItemWithCardData("me", " 4:55pm", "250", "cairo", true),
-            ItemWithCardData("me", " 4:55pm", "250", "cairo", true),
-            ItemWithCardData("me", " 4:55pm", "250", "cairo", false),
-            ItemWithCardData("ewewewewe", " 4:55pm", "250", "sdsdfdfdfds", true),
-            ItemWithCardData("me", " 4:55pm", "250", "cairo", true),
-            ItemWithCardData("me", " 4:55pm", "250", "cairo", true),
-            ItemWithCardData("me", " 4:55pm", "250", "cairo", false),
-            ItemWithCardData("ewewewewe", " 4:55pm", "250", "sdsdfdfdfds", true),
-            ItemWithCardData("me", " 4:55pm", "250", "cairo", true),
-        )
+//            ItemWithCardData("me", " 4:55pm", "250", "cairo", true),
+//            ItemWithCardData("me", " 4:55pm", "250", "cairo", false),
+//            ItemWithCardData("ewewewewe", " 4:55pm", "250", "sdsdfdfdfds", true),
+//            ItemWithCardData("me", " 4:55pm", "250", "cairo", true),
+//            ItemWithCardData("me", " 4:55pm", "250", "cairo", true),
+//            ItemWithCardData("me", " 4:55pm", "250", "cairo", false),
+//            ItemWithCardData("ewewewewe", " 4:55pm", "250", "sdsdfdfdfds", true),
+//            ItemWithCardData("me", " 4:55pm", "250", "cairo", true),
+//            ItemWithCardData("me", " 4:55pm", "250", "cairo", true),
+//            ItemWithCardData("me", " 4:55pm", "250", "cairo", false),
+//            ItemWithCardData("ewewewewe", " 4:55pm", "250", "sdsdfdfdfds", true),
+//            ItemWithCardData("me", " 4:55pm", "250", "cairo", true),
+
+            )
+
+        item.add(ItemWithCardData(addedText, addedTime, addedPlace, addedNotes, false))
+
         val clToast: ConstraintLayout? = view?.findViewById(R.id.clToast)
         if (item.isEmpty()) {
             Toast(context).apply {
